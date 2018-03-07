c1d1c9a31d154781b5b964a0d6c25e842d0a0182
Rahul Sheth <Rahul@s-169-232-241-251.resnet.ucla.edu>
2018-03-06 18:42:56 -0800

    UI changes to profile screen, event screen, and creation pipeline

---
+++ Screens/CreationScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Alert, DatePickerIOS, StyleSheet, Text, View, AppRegistry, Image, TextInput, Picker, Dimensions } from 'react-native';
+import { StackNavigator, } from 'react-navigation';
+import { List, ListItem, Button } from 'react-native-elements'; 
+
+import styles from './styles'; 
---
+++ constants.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, } from 'react-native';
+
+
+
+class myConstClass {
+
+
+
+const let DEFAULT_NAMES = [
+	{
+		name: 'Hao Nguyen', 
+		ImageLink: {BE_LOGO}, 
+	},
+	{
+		name: 'Rahul Sheth',
+		ImageLink: {BE_LOGO}, 
+	}, 
+	{
+		name: 'Hamilton Tran', 
+		ImageLink: {BE_LOGO}, 
+	},
+	{
+		name: 'Michael Yu',
+		ImageLink: {BE_LOGO},
+	}
+
+];
+}
