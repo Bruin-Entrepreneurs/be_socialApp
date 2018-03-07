25179736c13ece85f20f875cda333d285fa33d9c
Michael Yu <michael.yu2016@gmail.com>
2018-03-07 00:05:28 -0800

    make EventList functional/stateless component

--- components/CardList.js
+++ components/CardList.js
@@ -9,30 +9,22 @@ import {
 
 import styles from './styles/CardListStyle';
 
-class EventList extends React.Component {
-	constructor(){
-		super();
-	}
-
-	render() {
-		const cards = this.props.cards;
-
-		return (
-			<Card>
-				{cards.map((card, i) => {
-					return (
-		        <View key={i} style={styles.eventContainer}>
-		          <Image
-		          	style={styles.image}
-		            source={{uri: card.img}}
-	          	/>
-	          	<Text style={styles.text}>{card.title}</Text>
-		        </View>
-	        )}
-				)}
-			</Card>
-		);
-	}
+const EventList = ({cards}) => {
+	return (
+		<Card>
+			{cards.map((card, i) => {
+				return (
+	        <View key={i} style={styles.eventContainer}>
+	          <Image
+	          	style={styles.image}
+	            source={{uri: card.img}}
+          	/>
+          	<Text style={styles.text}>{card.title}</Text>
+	        </View>
+        )}
+			)}
+		</Card>
+	)
 }
 
 export default EventList;
