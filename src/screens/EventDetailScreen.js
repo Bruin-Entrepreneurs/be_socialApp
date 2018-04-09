62b3c2c2eb2f836c8f480698a77af7404dc3baba
Hao Nguyen <haolnguyen7@gmail.com>
2018-04-09 04:07:24 -0700

    accept decline

--- src/screens/EventDetailScreen.js
+++ src/screens/EventDetailScreen.js
@@ -8,93 +8,135 @@ import EventView from '../components/EventView';
 import styles from './styles/EventDetailScreen';
 
 export default class EventDetailScreen extends React.Component {
-	static navigationOptions = ({ navigation }) => ({
-		title: 'Event Detail'
-	})
+    static navigationOptions = ({ navigation }) => ({
+        title: 'Event Detail'
+    })
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			event: false,
-			superlike: false,
-			responded: false,
-			err: false,
-		}
+    constructor(props) {
+        super(props)
+        this.state = {
+            event: false,
+            superInvited: false,
+            responded: false,
+            err: false,
+        }
 
-		this._getEventAsync = this._getEventAsync.bind(this);
-	}
+        this._getEventAsync = this._getEventAsync.bind(this);
+    }
 
-	componentDidMount() {
-		const auth = storage.load({
-			key: 'auth',
-		}).then(
-			(auth) => this.setState(prevState => {
-				return Object.assign({}, prevState, 
-					{
-						auth: auth,
-					}
-				);
-			}, this._getEventAsync)
-		)
-	}
+    componentDidMount() {
+        const user = storage.load({
+            key: 'user',
+        }).then((user) => this.setState({ user: user }))
+        const auth = storage.load({
+            key: 'auth',
+        }).then(
+            (auth) => this.setState(prevState => {
+                return Object.assign({}, prevState,
+                    {
+                        auth: auth,
+                    }
+                );
+            }, this._getEventAsync)
+        )
+    }
 
-	render() {
-		const { navigate } = this.props.navigation;
-		const eventType = this.state.event.event_type;
-		console.log(this.state.event)
-		return (
-			<View style={styles.container}>
-				{
-					this.state.event ? (
-						<View style={{ flex: 1 }}>
-								<EventView title={eventType.name} desc={this.state.event.description} start_time={this.state.event.start_time} end_time={this.state.event.end_time} />
-								{!this.state.responded && <View style={styles.buttonContainer}>
-									<Button half title="Accept" />
-									<Button half title="Decline" />
-								</View>
-							}
-						</View>
-					) : (
-						<Text>Loading Event</Text>
-					)
-				}
-				{this.state.err && <Text>Error: {this.state.err}</Text>}
-			</View>
-		)
-	}
+    render() {
+        const { navigate } = this.props.navigation
+        const eventType = this.state.event.event_type
 
-	_getEventAsync = async () => {
-		const eventId = this.props.navigation.state.params.id;
-		const eventResponse = await fetch(
-			BASE_URL_PROD + '/event/' +  eventId,
-			{
-				method: 'GET',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-					Authorization: 'Bearer ' + this.state.auth.access_token
-				}
-			}
-		);
+        return (
+            <View style={styles.container}>
+                {
+                    this.state.event ? (
+                        <View style={{ flex: 1 }}>
+                            <EventView title={eventType.name} desc={this.state.event.description} start_time={this.state.event.start_time} end_time={this.state.event.end_time} />
+                            {!this.state.responded && <View style={styles.buttonContainer}>
+                                <Button half title="Accept" onPress={this._handleAccept} />
+                                <Button half title="Decline" onPress={this._handleDecline} />
+                            </View>
+                            }
+                        </View>
+                    ) : (
+                            <Text>Loading Event</Text>
+                        )
+                }
+                {this.state.err && <Text>Error: {this.state.err}</Text>}
+            </View>
+        )
+    }
 
-		const eventJson = await eventResponse.json();
+    _getEventAsync = async () => {
+        const eventId = this.props.navigation.state.params.id;
+        const eventResponse = await fetch(
+            BASE_URL_PROD + '/event/' + eventId,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + this.state.auth.access_token
+                }
+            }
+        )
 
-		if (eventResponse.ok) {
-			this.setState(prevState => {
-				return Object.assign({}, prevState, 
-					{
-						event: eventJson,
-					}
-				);
-			});
-		} else {
-			this.setState(prevState => {
-				return Object.assign({}, prevState, 
-					{
-						err: eventResponse.statusCode,
-					}
-				);
-			});
-		}
-	}
+        const eventJson = await eventResponse.json();
+
+        if (eventResponse.ok) {
+            this.setState(prevState => {
+                return Object.assign({}, prevState,
+                    {
+                        event: eventJson,
+                    }
+                );
+            });
+        } else {
+            this.setState(prevState => {
+                return Object.assign({}, prevState,
+                    {
+                        err: eventResponse.statusCode,
+                    }
+                );
+            });
+        }
+    }
+
+    _handleAccept = async () => {
+        const eventId = this.props.navigation.state.params.id
+        const acceptResponse = await fetch(
+            BASE_URL_PROD + '/event/' + eventId + '/accept/',
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + this.state.auth.access_token
+                }
+            }
+        )
+
+        if (acceptResponse.ok) {
+        } else {
+        }
+    }
+
+    _handleDecline = async () => {
+        const eventId = this.props.navigation.state.params.id
+        const declineResponse = await fetch(
+            BASE_URL_PROD + '/event/' + eventId + '/decline/',
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + this.state.auth.access_token
+                }
+            }
+        )
+
+        if (declineResponse.ok) {
+        } else {
+        }
+    }
+
 }
